c8c33c46a43e01859f13330211e790af2e77ff41
vallin1991 <victorvallinavila@gmail.com>
2022-05-03 14:55:23 -0700

    created the component header with the css and start working with the API for the dictionary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Container } from "@material-ui/core";
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [word, setWord] = useState("");
+
+  const [meanings, setMeanings] = useState([]);
+
+    const dictionaryApi = async() => {
+      try{
+        const data = await axios.get(
+          "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+        );
+        setMeanings(data.data);
+        
+      }catch (error){
+        console.log(error);
+      }
+    };
+    console.log(meanings);
+
+    useEffect(() => {
+      dictionaryApi();
+
+    }, [])
+   
+  return(
+     <div className="App" style={{height: "100vh", backgroundColor:"#282c34", color:"white"}}>
+    <Container maxWidth="md" style={{ display: "flex", flexDirection: "column", height:"100vh"}}>
+    <Header/>
+    </Container>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return(
+
+    <div className="header">
+        <span className="title"> Word Search</span>
+
+    </div>
+    );
+};
+
+
+export default Header;
